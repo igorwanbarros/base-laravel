f1c8cf34ccb17eb4a42566ba22f2abc6d742ef22
igorwanbarros <igorwanbarros@gmail.com>
2016-10-30 13:29:24 -0300

    adicionando metodos responsaveis por buscar o schema da tabela na classe BaseModel

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -3,9 +3,41 @@
 namespace Igorwanbarros\BaseLaravel\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class BaseModel extends Model
 {
+    protected static $informationSchema;
+
+
+    public static function getSchema()
+    {
+        if (env('DB_CONNECTION') != 'mysql') {
+            return [];
+        }
+
+        if (!isset(static::$informationSchema[get_called_class()])) {
+            return static::_initInformationSchema();
+        }
+
+        return static::$informationSchema[get_called_class()];
+    }
+
+
+    protected static function _initInformationSchema()
+    {
+        $static = new static;
+
+        $schema = DB::table('information_schema.columns')
+            ->where('table_name', '=', $static->getTable())
+            ->where('table_schema', '=', env('DB_DATABASE'))
+            ->get();
+
+        static::$informationSchema[get_called_class()] = $schema;
+
+        return $schema;
+    }
+
 
     public function saveOrUpdate(array $data = [])
     {
